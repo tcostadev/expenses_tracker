6c6a1c94a57cdc5d1e016b92caa187bb2a9ec2bf
tcostadev <49280479+tcostadev@users.noreply.github.com>
2026-01-07 09:11:01 +0000

    try to fix db migration

--- backend/Program.cs
+++ backend/Program.cs
@@ -24,13 +24,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Apply migrations on startup
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
